5ddef0cf5c34a1fae74fcd7327529f59fc39f36e
Joshweb <wongeubing@gmail.com>
2022-11-07 01:29:42 +0800

    Create homeworkw1.js
    Homework W1

---
+++ WongEuBing/homeworkw1.js
@@ -0,0 +1,124 @@
+// !The Age Calculator
+// let currentYear = 2022
+// let birthYear = 1986
+// let age = currentYear - birthYear
+// console.log(age)
+
+//! The Lifetime Supply Calculator
+// let currentAge = 36
+// let maximumAge = 42
+// let estimatedPerDay = 2
+// let eatTotal = (maximumAge - currentAge) * estimatedPerDay
+// console.log(eatTotal)
+// console.log("You will need ", eatTotal, " to last you until the ripe old age of ", maximumAge)
+
+// !The Geometrizer
+// let radius = 12
+// let circumference = (2 * 3.142 * Math.PI)
+// console.log("The circumference is", circumference)
+// let area = (3.142 * radius * radius)
+// console.log("The area is", area)
+
+// //! The Temperature Converter
+// let tempCelsius = 32
+// let convertToFarenheit = (tempCelsius * 1.8) + 32
+// console.log(tempCelsius, "\u00B0C is", convertToFarenheit, "\u00B0F")
+// let convertToCelsius = (convertToFarenheit - 32) * 0.56
+// console.log(convertToFarenheit, "\u00B0F is", convertToCelsius, "\u00B0C")
+
+//! The World Translator
+// let language = prompt("Please input language")
+
+// if (language == "english") {
+//     console.log("Hello World");
+// } else if (language == "french") {
+//     console.log("Bonjor le monde");
+// } else {
+//     console.log("Hai Dunia")
+// }
+
+//! The Grade Assigner
+// let testScore = prompt("Input Score")
+// if (testScore >= 8) {
+//     console.log("A");
+// } else if (testScore == 7) {
+//     console.log("B");
+// } else if (testScore >= 4 && testScore < 7) {
+//     console.log("C")
+// } else if (testScore < 4 && testScore >= 2) {
+//     console.log("D")
+// } else if (testScore < 2) {
+//     console.log("F")
+// }
+
+//! Air Conditioning
+// let temp = 32
+// let functional = true
+// let desiredTemp = 24
+
+// if (desiredTemp < temp && functional == true) {
+//     console.log("Turn on the A/C please");
+// } else if (desiredTemp < temp && functional == false) {
+//     console.log("Fix the A/C now! It's hot!");
+// } else if (desiredTemp >= temp && functional == false) {
+//     console.log("Fix the A/C whenever you have the chance... It's cool")
+// }
+
+//! You and Your Government
+// let age = prompt("Input Age")
+
+// if (age >= 35) {
+//     console.log("You can vote AND hold any place in government!");
+// } else if (age >= 25 && age < 35) {
+//     console.log("You can vote AND run for the Senate!");
+// } else if (age >= 18 && age < 25) {
+//     console.log("You can vote!")
+// } else {
+//     console.log("You can't vote yet")
+// }
+
+//! Golf
+// let score = 1
+// let par = true
+
+// if (score == -2 && par == true) {
+//     console.log("Eagle")
+// } else if (score == 0 && par == false) {
+//     console.log("Hole in One");
+// } else if (score == -1 && par == true) {
+//     console.log("Birdie");
+// } else if (score == 0 && par == true) {
+//     console.log("Par");
+// } else if (score == 1 && par == true) {
+//     console.log("Bogey");
+// } else if (score == 2 && par == true) {
+//     console.log("Double Bogey");
+// } else if (score == 3 && par == true) {
+//     console.log("Not Sure");
+// }
+
+//! Serge Says
+// let message = prompt("type message")
+
+// if (message.includes("?") == true) {
+//     console.log("Sure")
+// } else if (message.toUpperCase() == message && message != "") {
+//     console.log("Whoah, chill out!")
+// } else if (message == "") {
+//     console.log("Fine. Be that way!")
+// } else {
+//     console.log("Whatever")
+// }
+
+//! The Pluralizer
+//~ Not sure how to do this question
+
+//! The Rest
+//~ Not sure how to do this question
+
+//! Javascript Loops
+//~ Not enough time to try this homework. Will do this week. 
+
+
+
+
